45d998d107912d250ac5c0d0de73fd4e2dbb78ea
Laura Pham <l.pham2311@gmail.com>
2020-07-05 17:49:11 +1000

    Added comments on loading secure server

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,5 +1,10 @@
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+const video = document.querySelector(".player");
+const canvas = document.querySelector(".photo");
+const ctx = canvas.getContext("2d");
+const strip = document.querySelector(".strip");
+const snap = document.querySelector(".snap");
+
+// must use a secure server e.g. https or localhost to feed through index.html
+// package.json has browsersync dependency
+// step 1. npm install - installs packages
+// step 2. npm start - starts server
